3ad5e9c4595d55fdc6ce7cb472b7bb1c893f5777
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2025-11-21 19:11:39 -0500

    Time: 2 ms (84.34%), Space: 46.6 MB (12.28%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -14,45 +14,48 @@ class Solution {
     }
 
     private void backtrack(int r, char[][] board, List<List<String>> result){
+       
         if(r == board.length){
             List<String> copy = new ArrayList<>();
             for(char[] row : board){
-                copy.add(new String(row)); //basically add each row once we are finished;
+                copy.add(new String(row));
             }
+
             result.add(copy);
             return;
         }
 
-
-        for(int c = 0; c<board.length; c++){
+        for(int c = 0; c < board.length; c++){
             if(isSafe(r, c, board)){
-                board[r][c] = 'Q'; //place a queen
-                backtrack(r + 1, board, result); //move on to the next row;
-                board[r][c] = '.'; //explore other pocibilities.
+                board[r][c] = 'Q';
+                backtrack(r+1, board, result);
+                board[r][c] = '.';
             }
         }
+
 
     }
 
     private boolean isSafe(int r, int c, char[][] board){
 
-        //checking the rows above the current collumn, so moving up and checking if there is a Queen placed in there
-        for(int i = r-1; i>= 0; i--){
+        //up
+        for(int i = r-1; i>=0; i--){
             if(board[i][c] == 'Q') return false;
         }
 
-        //checking the diagonals, direction: right-bottom to left-top:
+        //diag right -> left -> up:
         for(int i = r-1, j = c-1; i >= 0 && j >= 0; i--, j--){
             if(board[i][j] == 'Q') return false;
         }
 
-        //checking the diagonal, direction: up and right, so think left-bottom corner to right-upper corner:
-        for(int i = r - 1, j = c + 1; i >= 00 && j < board.length; i--, j++){
+
+        //diag left -> right -> up:
+        for(int i = r-1, j = c+1; i >= 0 && j < board.length; i--, j++){
             if(board[i][j] == 'Q') return false;
         }
-        
-        return true;
 
+        return true;
+       
     }
 
 }
