7f2da1ab3e310decc12e6c9bc572e346ad14da7f
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2025-11-28 17:09:34 -0500

    Time: 76 ms (60.26%), Space: 47.5 MB (57.64%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,45 +1,40 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         
-
-        HashSet<String> set = new HashSet<>();
+        Set<String> set = new HashSet<>();
         for(String word : wordList){
             set.add(word);
         }
-        if(!set.contains(endWord)){
-            return 0;
-        }
-        Queue<String> queue = new LinkedList<>();
-        queue.offer(beginWord);
-        int level = 1;
+        if(!set.contains(endWord)) return 0;
 
+        int level = 1;
+        Queue<String> queue = new LinkedList<>();
+
+        queue.offer(beginWord);
+        
         while(!queue.isEmpty()){
 
             int size = queue.size();
-            for(int i = 0; i < size; i++){
-                String current_word = queue.poll();
-                char[] wordChars = current_word.toCharArray();
-                for(int j = 0; j<wordChars.length; j++){
-                    char originalChar = wordChars[j];
+            for(int i = 0; i<size; i++){
+                String currentWord = queue.poll();
+                char[] charArr = currentWord.toCharArray();
+                for(int j = 0; j<charArr.length; j++){
+                    char originalChar = charArr[j];
                     for(char c = 'a'; c<='z'; c++){
-                        if(wordChars[j] == c) continue;
-                        wordChars[j] = c;
-                        String newWord = String.valueOf(wordChars);
-                        if(newWord.equals(endWord)) return level + 1;
+                        if(charArr[j] == c) continue;
+                        charArr[j] = c;
+                        String newWord = String.valueOf(charArr);
+                        if(endWord.equals(newWord)) return level+1;
                         if(set.contains(newWord)){
                             queue.offer(newWord);
                             set.remove(newWord);
                         }
                     }
-                    wordChars[j] = originalChar;
-
+                    charArr[j] = originalChar;
                 }
             }
-
-            level++;
+            level+=1;
         }
-
-
         return 0;
     }
 }
