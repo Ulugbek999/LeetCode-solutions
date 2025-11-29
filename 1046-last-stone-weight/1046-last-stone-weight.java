b966e466c37c441d68bb38bf14f878f652647f70
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2025-11-28 19:19:36 -0500

    Time: 4 ms (2.41%), Space: 43 MB (12.5%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+        Queue<Integer> queue = new PriorityQueue<>(Comparator.comparing((Integer l) -> l).reversed());
+
+        for(int stone : stones){
+            queue.offer(stone);
+        }
+        int val = 0;
+        while(queue.size() > 1){
+            int heavierStone = queue.poll();
+            int lighterStone = queue.poll();
+            if(heavierStone == lighterStone){
+                continue;
+            }else{
+                val = Math.abs(heavierStone - lighterStone);
+                queue.offer(val);
+            }
+        }
+        queue.offer(0);
+        return queue.peek();
+        
+    }
+}
