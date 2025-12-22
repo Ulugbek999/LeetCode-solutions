40fac469dbae2f53132a4ce1b0e242076428b188
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2025-12-22 12:02:56 -0500

    Time: 276 ms (47.51%), Space: 142.5 MB (15.65%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,38 +1,35 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        
-        Queue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+
+        Queue<int[]> queue = new PriorityQueue<>(Comparator.comparing((int[] arr) -> arr[1]));
         boolean[] visited = new boolean[points.length];
-        int minimumCost = 0;
-        minHeap.offer(new int[]{0, 0});
-        
 
-        while(!minHeap.isEmpty()){
-            int[] current = minHeap.poll();
-            int vertex = current[0];
-            int weight = current[1];
+        queue.add(new int[]{0, 0});
+        int result = 0;
+        int connected = 0;
 
-            if (visited[vertex]){
-                continue;
-            }
+        while(connected < points.length){
+            int[] curr = queue.poll();
+            int u = curr[0];
+            int cost = curr[1];
+
+            if(visited[u]) continue;
+            int x = points[u][0];
+            int y = points[u][1];
             
-            visited[vertex] = true;
-            minimumCost+=weight;
-            
-            for(int i=0; i<points.length; i++){
-                if(!visited[i]){
-                    int distance = Math.abs(points[i][0] - points[vertex][0]) + Math.abs(points[i][1] - points[vertex][1]);
-                    minHeap.offer(new int[]{i, distance});
-                    
-                }
+            result += cost;
+            visited[u]= true;
+            connected++;
+
+            for(int v = 0; v<points.length; v++){
+
+                int newX = points[v][0];
+                int newY = points[v][1];
+
+                int newDist = Math.abs(x - newX) + Math.abs(y - newY);
+                queue.offer(new int[]{v, newDist});
             }
-        }   
-
-
-        return minimumCost;
-
-
-
-
+        }
+        return result;
     }
 }
