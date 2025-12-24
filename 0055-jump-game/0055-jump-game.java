ea81b3749baa04f85d54f6cabb73efb8b3250919
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2025-12-24 16:21:26 -0500

    Time: 1 ms (99.89%), Space: 48 MB (16.75%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -4,15 +4,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
 
-        int goal = nums.length-1;
+        int n = nums.length-1;
 
         for(int i = nums.length-2; i>= 0; i--){
-            if(i + nums[i] >= goal){
-                goal = i;
-            }
-        }
 
-        return goal == 0;
+            if(i + nums[i] >= n){
+                n = i;
+            }
+
+        }
+        return n == 0;
 
     }
 }
