eba30502979561352be4a991c180140c9cfd4ccc
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2025-12-24 16:09:32 -0500

    Time: 1 ms (99.78%), Space: 77.3 MB (24.37%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -2,21 +2,18 @@ class Solution {
     public int maxSubArray(int[] nums) {
         
 
+        int maxSum  = nums[0];
         int currentSum = 0;
-        int maxSum = nums[0];
-
         for(int num : nums){
+            currentSum += num;
+            maxSum = Math.max(maxSum, currentSum);
+
             if(currentSum < 0){
                 currentSum = 0;
             }
-            currentSum += num;
-            maxSum = Math.max(maxSum, currentSum);
+
         }
 
-
         return maxSum;
-
-
-
     }
 }
