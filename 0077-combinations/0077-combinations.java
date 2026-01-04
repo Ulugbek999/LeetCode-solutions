04747f23730cf5c8355d64922031c6f4d5dadedf
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-01-04 14:29:46 -0500

    Time: 21 ms (28.41%), Space: 98.3 MB (8.65%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,59 +1,35 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
 
-        List<List<Integer>> result = new ArrayList<>();
         int[] nums = new int[n];
+        int counter = 1;
         for(int i = 0; i<n; i++){
-            nums[i] = i+1;
+            nums[i] = counter++;
         }
 
-
-        backtrack(result, new ArrayList<>(), nums, k, 0);
-
-        return result;
-
-    }
-
-
-    // private List<Integer> otherOptions(List<Integer> state, int[] nums){
-    //     List<Integer> options = new ArrayList<>();
-    //     for(int num : nums){
-    //         if(!state.contains(num)){
-    //             options.add(num);
-    //         }
-    //     }
-    //     return options;
-    // } 
-
-
-    private void backtrack(List<List<Integer>> result, List<Integer> state, int[] nums, int k, int counter){
-        // System.out.println(state);
-        if(state.size() == k){
-            // List<Integer> sorted = new ArrayList<>(state);
-            // Collections.sort(sorted);
-            // if(!result.contains(sorted))
-            result.add(new ArrayList<>(state));
-            
-            return;
-        }
-
-
-        if(counter >= nums.length){
-            return;
-        }
-
-        state.add(nums[counter]);
-        backtrack(result, state, nums, k, counter+1);
-        state.remove(state.size() - 1);
-        backtrack(result, state, nums, k, counter+1);
-    
-        // for(int option : otherOptions(state, nums)){
-
-        //     state.add(option);
-        //     backtrack(result, state, nums, k);
-        //     state.remove(state.size() - 1);
+        // for(int num : nums){
+        //     System.out.println(num);
         // }
 
+        List<List<Integer>> list = new ArrayList<>();
+        backtrack(list, nums, 0, k, new ArrayList<>());
+
+        return list;
     }
 
+    private void backtrack(List<List<Integer>> list, int[] nums, int i, int k, List<Integer> sub){
+        // System.out.println(sub);
+        if(sub.size() == k){
+            list.add(new ArrayList<>(sub));
+            return;
+        }
+
+        if(i >= nums.length) return;
+
+        sub.add(nums[i]);
+        backtrack(list, nums, i+1, k, sub);
+        sub.remove(sub.size() - 1);
+        backtrack(list, nums, i+1, k, sub);
+
+    }
 }
