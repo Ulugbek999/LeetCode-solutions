1b773202fa899e0800d776bb5e3a6c1d7ebf276a
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-01-10 19:24:46 -0500

    Time: 2 ms (57.69%), Space: 42.4 MB (24.66%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -2,7 +2,6 @@ class Solution {
     public int totalNQueens(int n) {
     
 
-        List<List<String>> result = new ArrayList<>();
 
         char[][] board = new char[n][n];
 
@@ -11,40 +10,27 @@ class Solution {
                 board[i][j] = '.';
             }
         }
-        
-        backtrack(0, board, result);
+        int[] number = new int[1];
+        backtrack(0, board, number);
 
-        return result.size();
+        return number[0];
 
 
 
     }
     // char[][] = {{'.','.','Q','.'},{},{}}
 
-    private void backtrack(int row, char[][] board, List<List<String>> result){
-
-        // for(int i = 0; i<board.length; i++){
-        //     for(int j = 0; j<board[0].length; j++){
-        //         System.out.print(board[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        // System.out.println();
-
+    private void backtrack(int row, char[][] board, int[] number){
               
         if(row == board.length){
-            List<String> copy = new ArrayList<>();
-            for(char[] r : board){
-                copy.add(new String(r));
-            }
-            result.add(copy);
+                number[0]++;
             return;
         }
         
         for(int c = 0; c<board[0].length; c++){
             if(valid(row, c, board)){
                 board[row][c] = 'Q';
-                backtrack(row + 1, board, result);
+                backtrack(row + 1, board, number);
                 board[row][c] = '.';
             }
         }
