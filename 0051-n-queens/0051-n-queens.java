345457f24b4dfdd3ed67923bd563d504c738f53c
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-01-10 19:18:29 -0500

    Time: 1386 ms (5.63%), Space: 47.3 MB (5.51%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,61 +1,75 @@
 class Solution {
-
     public List<List<String>> solveNQueens(int n) {
+
         List<List<String>> result = new ArrayList<>();
+
         char[][] board = new char[n][n];
+
         for(int i = 0; i<n; i++){
             for(int j = 0; j<n; j++){
                 board[i][j] = '.';
             }
-        }    
+        }
+        
         backtrack(0, board, result);
+
         return result;
 
+
+
     }
+    // char[][] = {{'.','.','Q','.'},{},{}}
 
-    private void backtrack(int r, char[][] board, List<List<String>> result){
-       
-        if(r == board.length){
-            List<String> copy = new ArrayList<>();
-            for(char[] row : board){
-                copy.add(new String(row));
+    private void backtrack(int row, char[][] board, List<List<String>> result){
+
+        for(int i = 0; i<board.length; i++){
+            for(int j = 0; j<board[0].length; j++){
+                System.out.print(board[i][j] + " ");
             }
+            System.out.println();
+        }
+        System.out.println();
 
+              
+        if(row == board.length){
+            List<String> copy = new ArrayList<>();
+            for(char[] r : board){
+                copy.add(new String(r));
+            }
             result.add(copy);
             return;
         }
-
-        for(int c = 0; c < board.length; c++){
-            if(isSafe(r, c, board)){
-                board[r][c] = 'Q';
-                backtrack(r+1, board, result);
-                board[r][c] = '.';
+        
+        for(int c = 0; c<board[0].length; c++){
+            if(valid(row, c, board)){
+                board[row][c] = 'Q';
+                backtrack(row + 1, board, result);
+                board[row][c] = '.';
             }
         }
-
-
     }
 
-    private boolean isSafe(int r, int c, char[][] board){
-
-        //up
-        for(int i = r-1; i>=0; i--){
+    private boolean valid(int r, int c, char[][] board){
+        //to check the vertical
+        for(int i = r; i>=0;i--){
             if(board[i][c] == 'Q') return false;
         }
 
-        //diag right -> left -> up:
-        for(int i = r-1, j = c-1; i >= 0 && j >= 0; i--, j--){
+        //check the horizontal
+        for(int i = c; i>= 0; i--){
+            if(board[r][i] == 'Q') return false;
+        }
+
+        //check the left diagonal
+        for(int i = r, j = c; i >= 0 && j < board[0].length; i--, j++){
             if(board[i][j] == 'Q') return false;
         }
 
-
-        //diag left -> right -> up:
-        for(int i = r-1, j = c+1; i >= 0 && j < board.length; i--, j++){
+        //check the right diagonal
+        for(int i = r, j = c; i >= 0 && j >= 0; i--, j--){
             if(board[i][j] == 'Q') return false;
-        }
-
-        return true;
-       
+        }    
+        return true;   
     }
 
 }
