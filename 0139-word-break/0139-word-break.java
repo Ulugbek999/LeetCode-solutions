bc19588fc07cc31f3e45babf54b96a7a68eae367
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-01-11 19:52:00 -0500

    Time: 2 ms (85.17%), Space: 43.9 MB (70.05%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -7,16 +7,18 @@ public class Solution {
         memo = new HashMap<>();
         memo.put(s.length(), true);
         return dfs(s, wordDict, 0);
-
     }
 
     private boolean dfs(String s, List<String> wordDict, int i){
+        
+
         if(memo.containsKey(i)){
             return memo.get(i);
         }
 
         for(String w : wordDict){
             if(i + w.length() <= s.length() && s.substring(i, i+w.length()).equals(w)){
+                //System.out.println(s.substring(i, i+w.length()));
                 if(dfs(s, wordDict, i+w.length())){
                     memo.put(i, true);
                     return true;
