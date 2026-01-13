a63f64438c044aac371f77d374c25ae8d37dcb9f
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-01-12 19:27:57 -0500

    Time: 11 ms (8.56%), Space: 47.8 MB (6.3%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -0,0 +1,50 @@
+class Solution {
+
+
+
+    public int islandPerimeter(int[][] grid) {
+
+        int[][] positions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for(int i = 0; i<grid.length; i++){
+            for(int j = 0; j<grid[0].length; j++){
+                if(grid[i][j] == 1){
+                    queue.add(new int[]{i, j});
+                    visited[i][j] = true;
+                    break;
+                }
+            }
+        }
+
+        int per = 0;
+        while(!queue.isEmpty()){
+            int[] curr = queue.poll();
+            int r = curr[0];
+            int c = curr[1];
+
+
+            //visited[cr][cc] = true;
+
+            for(int[] pos : positions){
+
+                int cr = r + pos[0];
+                int cc = c + pos[1];
+
+                if(cr < 0 || cc < 0 || cr >= grid.length || cc >= grid[0].length){
+                    per++;
+                }else if(grid[cr][cc]==0){
+                    per++;
+                }else if(!visited[cr][cc]){
+                    visited[cr][cc] = true;
+                    queue.add(new int[]{cr, cc});
+                }
+            }
+        }
+
+        return per;
+
+
+    }
+}
