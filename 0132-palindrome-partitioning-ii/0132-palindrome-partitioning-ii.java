b3e52b86da21cbf8ef491813331039769c391b83
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-01-19 18:21:57 -0500

    Time: 34 ms (67.12%), Space: 47.9 MB (17.87%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,40 +1,31 @@
 class Solution {
-
     public int minCut(String s) {
-
+        
         int n = s.length();
-        int result = 0;
+        int[] dp = new int[n];
         boolean[][] isPal = new boolean[n][n];
 
-        //precomputing palindrome numbers:
-        for(int i = n-1; i >= 0; i--){
-            for(int j = i; j < n; j++){
 
-                if(s.charAt(i) == s.charAt(j) && (j - i <= 2 || isPal[i + 1][j-1])){
+        //Phase 1: Computer all of the palindromic substrings
+        for(int i = n-1; i>= 0; i--){
+            for(int j = i; j<n;j++){
+                if(s.charAt(i) == s.charAt(j) && (j-i <= 2 || isPal[i+1][j-1] == true)){
                     isPal[i][j] = true;
-                    // System.out.println("i = " + i + " j = " + j );
                 }
             }
         }
+        //the palindromic substrings are calculate, now calculate minimum cuts.
 
-
-        //Step 2: dp for minimum cuts
-        int[] dp = new int[n];
+        //Phase 2: Calculate the minimum number of cuts;
         for(int i = 0; i<n; i++){
-            dp[i] = i;//worst case
-
-            for(int j = 0; j<=i; j++){
+            //initialize the worst case where we have cut before each character:
+            dp[i] = i;
+            for(int j = 0; j<=i;j++){
                 if(isPal[j][i]){
                     dp[i] = (j==0) ? 0 : Math.min(dp[i], dp[j-1] + 1);
                 }
             }
         }
-
-        
-
-        
         return dp[n-1];
-
     }
-
 }
