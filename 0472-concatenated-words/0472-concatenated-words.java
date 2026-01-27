5053e20dfe31fcb1bdce6a327001bd795edd915a
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-01-27 10:36:28 -0500

    Time: 257 ms (5.01%), Space: 52.9 MB (45.24%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -0,0 +1,49 @@
+class Solution {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+
+        List<String> result = new ArrayList<>();
+        Set<String> dict = new HashSet<>();
+
+        int maxLen = 0;
+        for(String w : words){
+            maxLen = Math.max(w.length(), maxLen);
+            dict.add(w);
+
+
+        }
+
+        for(String word : words){
+
+            boolean[] dp = new boolean[word.length() + 1];
+            dp[word.length()] = true;
+
+            for(int i = word.length()-1; i >= 0; i--){
+
+
+                for(int j = i + 1; j<=word.length() && j- i <= maxLen; j++){
+
+                    String sub = word.substring(i, j);
+
+                    if(!sub.equals(word) && dict.contains(sub)){
+                        dp[i] = dp[j];
+                    }
+
+                    if(dp[i]) break;
+
+                }
+
+
+            }
+
+            if(dp[0] == true){
+                result.add(word);
+            }
+
+
+        }
+
+        return result;
+        
+        
+    }
+}
