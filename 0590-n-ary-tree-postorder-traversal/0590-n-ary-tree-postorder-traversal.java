b680877476dad40118595451cf9f7016e194679f
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-01-28 21:30:36 -0500

    Time: 1 ms (42.74%), Space: 46.7 MB (11.08%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+
+    List<Integer> result = new ArrayList<>();
+
+    public List<Integer> postorder(Node root) {
+        
+        //List<Integer> result = new ArrayList<>();
+        dfs(root);
+        return result;
+
+    }
+
+    private void dfs(Node root){
+
+        if(root == null) return;
+
+        for(Node node : root.children){
+            dfs(node);
+        }
+
+        result.add(root.val);
+
+    }
+}
