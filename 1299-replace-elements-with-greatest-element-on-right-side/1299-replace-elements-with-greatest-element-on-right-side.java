2b8a18d35d58d4c3bbf0eb9aa15ff36bc62e0908
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-01-31 11:46:20 -0500

    Time: 240 ms (29.56%), Space: 49.9 MB (9.35%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,22 +1,17 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
 
+        int[] dp = new int[arr.length];
 
+        dp[dp.length-1] = -1;
+        int runningMax = 0;
+        int j = dp.length-2;
+        for(int i = arr.length-1;i>0;i--){
+            runningMax = Math.max(runningMax, arr[i]);
+            dp[j--] = runningMax;
+        }   
 
-        for(int i = 0; i<arr.length; i++){
-            int max = 0;
-            for(int j = i+1;j<arr.length;j++){
-                max = Math.max(max, arr[j]);
-            }
-            arr[i] = max;
-        }
+        return dp;
 
-        arr[arr.length-1] = -1;
-
-        return arr;
-
-
-
-        
     }
 }
