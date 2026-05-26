7edb2e682bb67b6f8b779843740d5d5e4674b898
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-05-25 21:34:29 -0400

    Time: 11 ms (22.56%), Space: 3.7 MB (69.8%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.rs
@@ -0,0 +1,18 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        
+        let mut set: HashSet<i32> = HashSet::new();
+
+        for i in 0..nums.len() {
+            if(set.contains(&nums[i])) {
+                return true;
+            }
+            set.insert(nums[i]);
+        }
+
+        return false;
+
+    }
+}
