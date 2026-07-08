d3d1fc0840c5224548a83847416fc2698cf8030b
Bek Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-07-07 23:34:30 -0400

    Time: 40 ms (1.01%), Space: 46.1 MB (10.65%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -0,0 +1,29 @@
+
+
+class Solution {
+
+    public int minCostClimbingStairs(int[] cost) {
+
+        int n = cost.length;
+
+        int[] dp = new int[n+1];
+
+        //Becasue it would take 0 currency to jump from before the staircase on either first or second step.
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for(int i = 2; i<=n; i++){
+            dp[i] = Math.min(
+                cost[i-1] + dp[i-1],
+                cost[i-2] + dp[i-2]
+            );
+        }
+
+        for(int num : dp){
+            System.out.print(" " + num);
+        }
+
+        return dp[n];
+
+    }
+}
