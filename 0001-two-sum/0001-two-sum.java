555f107ce55c047eaf7f21bbe768c871db141b96
Beck Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-08-11 22:22:46 -0400

    Time: 2 ms (99.34%), Space: 47.2 MB (23.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,23 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        
+
+        Map<Integer, Integer> map = new HashMap<>();
+
 
         for(int i = 0; i<nums.length; i++){
-            for(int j = i+1; j<nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i, j};
-                }
+
+            
+            int compliment = target - nums[i];
+
+
+            if(map.containsKey(nums[i])){
+                return new int[]{map.get(nums[i]), i};
+            }else{
+                map.put(compliment, i);
             }
         }
+
         return new int[]{-1, -1};
     }
 }
