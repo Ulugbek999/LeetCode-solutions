e96c1c1ac4517e4009b601fe6092798ec5f06a83
Beck Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-08-11 22:45:06 -0400

    Time: 16 ms (37.87%), Space: 47.7 MB (33.15%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,36 +1,29 @@
 class Solution {
-
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
 
-        if(nums.length == 0){
-            return new int[]{};
-        }
+
+        Map<Integer, Integer> map = new HashMap<>();
 
         for(int num : nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        Queue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(Comparator.comparing((Map.Entry<Integer, Integer> entry) -> entry.getValue()).reversed());
+        //System.out.println(map);
 
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            pq.offer(entry);
+        Comparator<Map.Entry<Integer, Integer>> sortBasedOnFrequency = Comparator.comparingInt((Map.Entry<Integer, Integer> entry) -> entry.getValue()).reversed();
+
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(sortBasedOnFrequency);
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            queue.offer(entry);
         }
-
         int[] result = new int[k];
-       
-        int iterator = 0;
-        while(iterator < k){
-        // Map.Entry<Integer, Integer> entry  = pq.poll();
-        // result[iterator] = entry.getKey();
-        // // iterator++;
-            result[iterator++] = pq.poll().getKey();
+        int i = 0;
+        while(i < k){
+            result[i++] = queue.poll().getKey();
         }
 
         return result;
+
+        
     }
-
-
 }
-
-
