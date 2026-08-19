fa8f3d7310f9520b6921e9df3406f20516ba3bfa
Beck Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-08-19 11:53:36 -0400

    Time: 35 ms (59.17%), Space: 73.5 MB (41.24%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,14 +1,14 @@
-
-
 class Solution {
 
-    int[][] jobs;
-    int[] memo;
+    int[][] jobs; //an array of arrays to keep each start->end->profit or each job
+    int[] memo;//to store the max profit
+
+
 
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-
-        int n = startTime.length;
-        jobs = new int[n][3]; //[[start][end][profit]];
+        
+        int n = startTime.length; //a variable to have the number of jobs
+        jobs = new int[n][3]; //
 
         for(int i = 0; i<n; i++){
             jobs[i][0] = startTime[i];
@@ -16,49 +16,58 @@ class Solution {
             jobs[i][2] = profit[i];
         }
 
-        //Sorting the jobs by their start time
+
+        //sorting the jobs by startTime in ascending order
         Arrays.sort(jobs, (a, b) -> Integer.compare(a[0], b[0]));
 
-        //memo -> an array to store maximum profti
+
         memo = new int[n];
-        Arrays.fill(memo, -1); //cause 0 could be a legitimate answer;
+        //Fill the memo with -1s initially, since the profit could potentially be = 0, hence why we are not filling the array with 0s.
+        Arrays.fill(memo, -1);
 
-        return solve(0);
 
+        return  solve(0);
+        
     }
 
+    //recursive function that also uses memoization to find the highest possible profit
     private int solve(int i){
-        //base case:
+
+        //base case to stop the recursion
         if(i >= jobs.length){
             return 0;
         }
 
         if(memo[i] != -1){
-            //then this subproblem has been already solved
+            //means we have already computer this value before, return the value we have computed:
             return memo[i];
         }
 
-        //Option 1: SKIP the current job
+        //two possible scenarios
+        //case one: We skip the current value and go to the next one:
         int skip = solve(i+1);
 
-        //Otion 2: Take the current job
-        int next = findNextJob(i);
+        //case two: We take the current value and based on it, we add the next possible value:
+        int next = nextAvailableJob(i);
+
+        //take the max of each:
         int take = jobs[i][2] + solve(next);
 
+        //memoize the maximum value
         memo[i] = Math.max(skip, take);
 
+        
         return memo[i];
 
     }
 
 
-    //binary search to find the next available job
-    private int findNextJob(int i){
+    //binary search through the jobs to identify the next possible job, given that we selected the current job at i
+    private int nextAvailableJob(int i){
 
-
-        int currentEnd = jobs[i][1];
-        int left = i + 1;
+        int currentEnd = jobs[i][1];//the end of the current job
         int right = jobs.length;
+        int left = i + 1;
 
         while(left < right){
             int mid = left + (right - left)/2;
@@ -67,11 +76,15 @@ class Solution {
             }else{
                 left = mid + 1;
             }
+
         }
 
         return left;
+        
+
 
     }
+
 
 
 }
