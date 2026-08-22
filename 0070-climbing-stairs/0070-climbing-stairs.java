da5fa547a6eea2c0da1a01b3f628aec860dff740
Beck Abdurakhmonov <81351768+Ulugbek999@users.noreply.github.com>
2026-08-22 13:17:12 -0400

    Time: 0 ms (100%), Space: 42 MB (73.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,24 @@
 class Solution {
     public int climbStairs(int n) {
-
-        int[] result = new int[n+1];
-        result[0] = 1;
-        result[1] = 2;
-
-        for (int i = 2; i<=n; i++){
-            result[i] = result[i-1] + result[i-2];
+        
+        if(n == 1 || n == 1){
+            return 1;
         }
 
 
-        return result[n-1];
+        int[] result = new int[n+1];
+        result[0] = 1;
+        result[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+
+            result[i] = result[i-2] + result[i-1]; 
+            //System.out.println(result[i]);
+
+        }
         
+        return result[n-2] + result[n-1];
+
     }
+     
 }
